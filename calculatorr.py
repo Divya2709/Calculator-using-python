222e9401a5c8a507fd6ecf78836462a6e0a761f4
Divya Raju Bawankar <dbawankar27@gmail.com>
2020-06-13 19:55:37 +0530

    Add files via upload

---
+++ calculatorr.py
@@ -0,0 +1,176 @@
+from tkinter import *
+root = Tk()
+root.title("Calculator using Python for user")
+root.resizable(0,0)
+
+e = Entry(root,font=("arial",40,"bold"),justify= 'right',bd=35,bg='Black',fg="White")
+e.grid(row=0, column=0 ,columnspan=5)
+
+#onclick_function
+def button_click(num):
+    present = e.get()
+    e.delete(0,END)
+    e.insert(0,str(present) + str(num))
+
+#clear function
+def button_clear():
+    e.delete(0,END)
+
+#addfunction
+def add_func():
+        first_num = e.get()
+        global f_num
+        global math
+        math = "addition"
+        f_num = float(first_num)
+        e.delete(0,END)
+
+#Equalfunction()
+def equal_func():
+    sec_num = e.get()
+    e.delete(0,END)
+
+    if math =="addition":
+        e.insert(0,(f_num) + float(sec_num))
+
+    if math =="Substraction":
+        e.insert(0,(f_num) - float(sec_num))
+
+    if math =="multiplication":
+        e.insert(0,f_num * float(sec_num))
+
+
+    if math =="division":
+        e.insert(0,f_num / float(sec_num))
+
+
+#substractfun
+def substract_func():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "Substraction"
+    f_num = float(first_num)
+    e.delete(0,END)
+
+#mul_function
+def mul_func():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = float(first_num)
+    e.delete(0,END)
+
+
+#divisionfunction
+def div_func():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = float(first_num)
+    e.delete(0,END)
+
+#squarefunction
+def square_func():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "Square"
+    f_num = float(first_num)
+    e.delete(0,END)
+    if math =="Square":
+        e.insert(0,f_num * f_num)
+
+#cubefunction
+def cube_func():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "Cube"
+    f_num = float(first_num)
+    e.delete(0,END)
+    if math =="Cube":
+        e.insert(0,f_num * f_num  * f_num)
+
+
+#squarerootfunction
+def sqroot_func():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "Squareroot"
+    f_num = float(first_num)
+    e.delete(0,END)
+    if math =="Squareroot":
+        e.insert(0,f_num ** 0.5)
+
+
+#exponentialfunction
+def exponential_func():
+    first_num = e.get()
+    global f_num
+    global math
+    math = "exponential"
+    f_num = float(first_num)
+    e.delete(0,END)
+    if math == "exponential":
+        e.insert(0,2.7182818285**f_num)
+
+
+
+#buttons
+button_clear = Button(root, text="Clear",padx=45,pady=20,font=("arial"), command=button_clear,bg="yellow")
+button_exponential = Button(root, text="e(x)",padx=48,pady=20,font=("arial"), command=exponential_func,bg="#ff1aff")
+button_squareroot = Button(root, text="√x",padx=50,pady=20,font=("arial"), command=sqroot_func,bg="#ff33ff")
+button_division = Button(root, text="/",padx=50,pady=20,font=("arial"), command=div_func,bg="#3399ff")
+number_7 = Button(root, text="7",padx=60,pady=20,font=("arial"), command=lambda:button_click(7),bg="#4dff4d")
+number_8 = Button(root, text="8",padx=56,pady=20,font=("arial"), command=lambda:button_click(8),bg="#4dff4d")
+number_9 = Button(root, text="9",padx=53,pady=20,font=("arial"), command=lambda:button_click(9),bg="#4dff4d")
+button_mul = Button(root, text="*",padx=49,pady=20,font=("arial"), command=mul_func,bg="#3399ff")
+number_4 = Button(root, text="4",padx=60,pady=20,font=("arial"), command=lambda:button_click(4),bg="#4dff4d")
+number_5 = Button(root, text="5",padx=56,pady=20,font=("arial"), command=lambda:button_click(5),bg="#4dff4d")
+number_6 = Button(root, text="6",padx=53,pady=20,font=("arial"), command=lambda:button_click(6),bg="#4dff4d")
+button_minus = Button(root, text="-",padx=49,pady=20,font=("arial"), command=substract_func,bg="#3399ff")
+number_1 = Button(root, text="1",padx=60,pady=20,font=("arial"), command=lambda:button_click(1),bg="#4dff4d")
+number_2 = Button(root, text="2",padx=56,pady=20,font=("arial"), command=lambda:button_click(2),bg="#4dff4d")
+number_3 = Button(root, text="3",padx=53,pady=20,font=("arial"), command=lambda:button_click(3),bg="#4dff4d")
+button_addition = Button(root, text="+",padx=47.5,pady=20,font=("arial"), command=add_func,bg="#3399ff")
+number_0 = Button(root, text="0",padx=60,pady=20,font=("arial"), command=lambda:button_click(0),bg="#4dff4d")
+button_xsq = Button(root, text="x²",padx=54,pady=20,font=("arial"), command=square_func,bg="#ff1aff")
+button_xcub= Button(root, text="x³",padx=52,pady=20,font=("arial"), command=cube_func,bg="#ff1aff")
+button_equal = Button(root, text="=",padx=50,pady=160,font=("arial"), command=equal_func,bg="#ff1a1a")
+button_decimal = Button(root, text=".",padx=50,pady=20,font=("arial"), command=lambda:button_click("."),bg="#3399ff")
+
+
+
+#row1
+button_clear.grid(row=1,column=0)
+button_exponential.grid(row=1,column=1)
+button_squareroot.grid(row=1,column=2)
+button_division.grid(row=1,column=3)
+button_equal.grid(row=1,column=4,rowspan=5)
+#row2
+number_7.grid(row=2,column=0)
+number_8.grid(row=2,column=1)
+number_9.grid(row=2,column=2)
+button_mul.grid(row=2,column=3)
+#row3
+number_4.grid(row=3,column=0)
+number_5.grid(row=3,column=1)
+number_6.grid(row=3,column=2)
+button_minus.grid(row=3,column=3)
+#row4
+number_1.grid(row=4,column=0)
+number_2.grid(row=4,column=1)
+number_3.grid(row=4,column=2)
+button_addition.grid(row=4,column=3)
+#row4
+number_0.grid(row=5,column=0)
+button_xsq.grid(row=5,column=1)
+button_xcub.grid(row=5,column=2)
+#row5
+button_decimal.grid(row=5,column=3)
+
+root.mainloop()
